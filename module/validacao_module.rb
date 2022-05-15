457ebce6beff95c0d50b1476d7b3d53e21f93fde
Kayque Prado <46896994+kayqueprado580@users.noreply.github.com>
2022-05-14 21:34:54 -0300

    Delete validacao_module.rb

--- module/validacao_module.rb
+++
@@ -1,9 +0,0 @@
-module Validacao
-  def valida_input(input)
-    true if input.nil? || input == ''
-  end
-
-  def valida_opcoes(input)
-    true if input != '1' && input != '2' && input != '3'
-  end
-end
